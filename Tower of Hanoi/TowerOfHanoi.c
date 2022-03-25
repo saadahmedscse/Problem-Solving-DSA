40a8c850943b3c80006625ccd89e39e8c31135d5
Saad Ahmed <saadahmedscse@gmail.com>
2022-03-25 20:38:33 +0600

    Problem "Tower of Hanoi"

--- Tower of Hanoi/TowerOfHanoi.c
+++ Tower of Hanoi/TowerOfHanoi.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
+/*
+    Created by Saad Ahmed on 25 March, 2022
+    Time Complexity of this program is "O(2^n)"
+*/
+
 void towerOfHanoi(int n, char a, char b, char c);
 
-int main(){
-    int n=2;
+int main()
+{
+    int n = 2;
     towerOfHanoi(n, 'A', 'B', 'C');
     return 0;
 }
 
-void towerOfHanoi(int n, char a, char b, char c){
-    if (n == 1){
+void towerOfHanoi(int n, char a, char b, char c)
+{
+    if (n == 1)
+    {
         printf("Transfered disk %d from %c to %c\n", n, a, c);
         return;
     }
 
-    towerOfHanoi(n-1, a, c, b);
+    towerOfHanoi(n - 1, a, c, b);
     printf("Transfered disk %d from %c to %c\n", n, a, c);
-    towerOfHanoi(n-1, b, a, c);
+    towerOfHanoi(n - 1, b, a, c);
 }
